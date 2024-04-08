d9ca8ddaa3f3ce654123e06208b127310da59999
Robbie-Kim <robbie6673@naver.com>
2024-04-08 21:51:53 +0900

    Refactor : 백엔드 서버로 요청 cors 오류 해결

--- src/main/java/com/bit/envdev/controller/MemberController.java
+++ src/main/java/com/bit/envdev/controller/MemberController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("api/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
