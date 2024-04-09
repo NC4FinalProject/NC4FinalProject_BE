b78bb7f501b9c6ac113e052f020257a55821ef86
deonkeon41 <deonkeon41@gmail.com>
2024-04-09 14:22:02 +0900

    feat : 매핑구조 변경에 따른 Payment, Cart 관련 로직변경

--- src/main/java/com/bit/envdev/entity/InquiryCommentLikeId.java
+++ src/main/java/com/bit/envdev/entity/InquiryCommentLikeId.java
@@ -11,4 +11,20 @@ import java.io.Serializable;
 public class InquiryCommentLikeId implements Serializable {
     private long inquiryComment;
     private long member;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof InquiryCommentLikeId)) return false;
+        InquiryCommentLikeId that = (InquiryCommentLikeId) o;
+        return inquiryComment == that.inquiryComment && member == that.member;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (int) (inquiryComment ^ (inquiryComment >>> 32));
+        result = 31 * result + (int) (member ^ (member >>> 32));
+        return result;
+    }
 }
