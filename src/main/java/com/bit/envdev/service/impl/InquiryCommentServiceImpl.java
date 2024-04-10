962cfd798f021d9ce0fc0b3ae4690466a4490db2
deonkeon41 <deonkeon41@gmail.com>
2024-04-10 22:43:01 +0900

    feat : 질의응답 댓글관련 매개변수 수정

--- src/main/java/com/bit/envdev/service/impl/InquiryCommentServiceImpl.java
+++ src/main/java/com/bit/envdev/service/impl/InquiryCommentServiceImpl.java
@@ -2,10 +2,13 @@ package com.bit.envdev.service.impl;
 
 import com.bit.envdev.dto.InquiryCommentDTO;
 import com.bit.envdev.entity.CustomUserDetails;
+import com.bit.envdev.entity.Inquiry;
 import com.bit.envdev.repository.InquiryCommentRepository;
+import com.bit.envdev.repository.InquiryRepository;
 import com.bit.envdev.repository.MemberRepository;
 import com.bit.envdev.service.InquiryCommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,13 +20,18 @@ import java.util.stream.Collectors;
 public class InquiryCommentServiceImpl implements InquiryCommentService {
     private final InquiryCommentRepository inquiryCommentRepository;
     private final MemberRepository memberRepository;
+    private final InquiryRepository inquiryRepository;
 
     @Override
-    public List<InquiryCommentDTO> post(InquiryCommentDTO inquiryCommentDTO, CustomUserDetails customUserDetails) {
+    public List<InquiryCommentDTO> post(long inquiryId, InquiryCommentDTO inquiryCommentDTO, CustomUserDetails customUserDetails) {
+
+        if (!inquiryRepository.existsById(inquiryId)) {
+            throw new RuntimeException("해당 문의글이 존재하지 않습니다.");
+        }
 
         inquiryCommentDTO.setMemberDTO(memberRepository.findById(customUserDetails.getMember().getMemberId()).orElseThrow().toDTO());
         inquiryCommentRepository.save(inquiryCommentDTO.toEntity());
-        return inquiryCommentRepository.findByInquiryInquiryIdOrderByInquiryCommentCrtDTDesc(inquiryCommentDTO.getInquiryCommentId()).stream()
+        return inquiryCommentRepository.findByInquiryInquiryIdOrderByInquiryCommentCrtDTDesc(inquiryId).stream()
                 .map(inquiryComment -> {
                     InquiryCommentDTO dto = inquiryComment.toDTO();
                     dto.getMemberDTO().setPassword("");
@@ -33,19 +41,33 @@ public class InquiryCommentServiceImpl implements InquiryCommentService {
     }
 
     @Override
-    public List<InquiryCommentDTO> modify(InquiryCommentDTO inquiryCommentDTO, CustomUserDetails customUserDetails) {
-        return null;
+    @Modifying
+    public List<InquiryCommentDTO> modify(long inquiryId, InquiryCommentDTO inquiryCommentDTO, CustomUserDetails customUserDetails) {
+        if (!inquiryRepository.existsById(inquiryId)) {
+            throw new RuntimeException("해당 문의글이 존재하지 않습니다.");
+        }
+
+        inquiryCommentDTO.setMemberDTO(memberRepository.findById(customUserDetails.getMember().getMemberId()).orElseThrow().toDTO());
+        inquiryCommentRepository.save(inquiryCommentDTO.toEntity());
+        return inquiryCommentRepository.findByInquiryInquiryIdOrderByInquiryCommentCrtDTDesc(inquiryId).stream()
+                .map(inquiryComment -> {
+                    InquiryCommentDTO dto = inquiryComment.toDTO();
+                    dto.getMemberDTO().setPassword("");
+                    return dto;
+                })
+                .collect(Collectors.toList());
     }
 
     @Override
-    public List<InquiryCommentDTO> delete(long inquiryCommentId, CustomUserDetails customUserDetails) {
-        return null;
+    public List<InquiryCommentDTO> delete(long inquiryId, long inquiryCommentId, CustomUserDetails customUserDetails) {
+
+        inquiryCommentRepository.deleteById(inquiryCommentId);
+        return inquiryCommentRepository.findByInquiryInquiryIdOrderByInquiryCommentCrtDTDesc(inquiryId).stream()
+                .map(inquiryComment -> {
+                    InquiryCommentDTO dto = inquiryComment.toDTO();
+                    dto.getMemberDTO().setPassword("");
+                    return dto;
+                })
+                .collect(Collectors.toList());
     }
-
-    @Override
-    public List<InquiryCommentDTO> getInquiryCommentList(int inquiryCommentId) {
-        return null;
-    }
-
-
 }
